ccf358e00d55a50b5e35585f61d25ee49ca8be54
João Gabriel <joaogabriel6392@gmail.com>
2026-07-02 14:36:53 -0300

    Add files via upload

---
+++ SistemaDeDeliveryDeRestaurante-Trycatch/src/main/java/Sistema/application/Program.java
@@ -0,0 +1,41 @@
+package Sistema.application;
+
+import Sistema.entities.contas.Login;
+
+import java.util.Scanner;
+
+import Sistema.entities.enums.Pagamento;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("E-mail: ");
+            String email = sc.nextLine();
+            System.out.print("Senha: ");
+            String senha = sc.nextLine();
+            Login login = new Login(email, senha);
+            login.setEmail(email);
+            login.setSenha(senha);
+            login.loginConta();
+            System.out.println();
+            login.cardapio();
+            System.out.println();
+            System.out.println("Qual prato deseja escolher? ");
+            String pratoEscolha = sc.nextLine();
+            Login escolha = new Login(pratoEscolha);
+            escolha.setPratoEscolha(pratoEscolha);
+            escolha.pedidoCalculo();
+            System.out.println();
+            escolha.cardapioEscolha(sc);
+            login.pagamentos();
+            String pagamento = sc.nextLine();
+            login.pagamentoDefinicao(pagamento);
+            login.recibo();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ SistemaDeDeliveryDeRestaurante-Trycatch/src/main/java/Sistema/entities/contas/Login.java
@@ -0,0 +1,159 @@
+package Sistema.entities.contas;
+
+
+import Sistema.entities.enums.Pagamento;
+
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class Login {
+    private final Random rnd = new Random();
+    private String email;
+    private String senha;
+    private final String[] emails = {"0001193455@senaimgaluno.com.br", "joao"};
+    private final String[] senhas = {"Hd87555", "bubu"};
+    private Cardapio cardapio;
+    private String pratoEscolha;
+    private String pratoEscolha2;
+    private  int totalPedido = 0;
+    private final Pagamento[] pagamentos = Pagamento.values();
+
+    public Login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+        this.cardapio = new Cardapio();
+    }
+
+    public Login(String pratoEscolha) {
+        this.pratoEscolha = pratoEscolha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha.trim();
+    }
+
+    public void setPratoEscolha(String pratoEscolha) {
+        this.pratoEscolha = pratoEscolha.toLowerCase();
+    }
+
+    public String getPratoEscolha() {
+        return pratoEscolha;
+    }
+
+    public String getPratoEscolha2() {
+        return pratoEscolha2;
+    }
+
+    public void setPratoEscolha2(String pratoEscolha2) {
+        this.pratoEscolha2 = pratoEscolha2;
+    }
+
+    public int getTotalPedido() {
+        return totalPedido;
+    }
+
+    public void setTotalPedido(int totalPedido) {
+        this.totalPedido = totalPedido;
+    }
+
+
+    public void loginConta() {
+        boolean correto = false;
+
+        for (int i = 0; i < emails.length; i++) {
+            if (getEmail().equals(emails[i]) && getSenha().equals(senhas[i])) {
+                correto = true;
+                break;
+            }
+        }
+
+        if (correto) {
+            System.out.println("Bem vindo!");
+        } else {
+            System.out.println("Usuário ou senha inválidos");
+            System.exit(0);
+        }
+
+    }
+
+    public void cardapio() {
+        for (String dishes : cardapio.getOpcoes()) {
+            if (!dishes.isEmpty()) {
+                System.out.println(cardapio);
+                break;
+            }
+        }
+
+    }
+
+    public void pedidoCalculo() {
+        if (pratoEscolha.equals("porcao de camarao")) {
+            setTotalPedido(totalPedido = 30);
+        } else if (pratoEscolha.equals("lasanha")) {
+            setTotalPedido(totalPedido = 25);
+        } else if (pratoEscolha.equals("almondega")) {
+            setTotalPedido(totalPedido = 15);
+        }
+    }
+
+    //Dar seguimento na seleção e calculo total do pedido-JJ
+    public void cardapioEscolha(Scanner sc) {
+        do {
+            System.out.println("O que mais? ");
+            pratoEscolha2 = sc.nextLine();
+            switch (pratoEscolha2) {
+                case "porcao de camarao" -> setTotalPedido(totalPedido += 30);
+                case "lasanha" -> setTotalPedido(totalPedido += 25);
+                case "almondega" -> setTotalPedido(totalPedido += 15);
+            }
+            System.out.println("Deseja pedir mais alguma coisa? ");
+            pratoEscolha2 = sc.nextLine();
+        } while (pratoEscolha2.equalsIgnoreCase("sim"));
+        System.out.println("Total do pedido: " + getTotalPedido());
+        if (getTotalPedido() < 50) {
+            System.out.println("Total com frete: " + (totalPedido += 8));
+            System.out.println();
+        }
+    }
+
+    public void pagamentos() {
+        System.out.println("Escolha sua forma de pagamento: ");
+        for (Pagamento pagamentoOpcoes : pagamentos) {
+            System.out.println(pagamentoOpcoes);
+        }
+    }
+
+    public void pagamentoDefinicao(String pagamento) {
+        if (pagamento.equalsIgnoreCase("pix") || pagamento.equalsIgnoreCase("cartao") || pagamento.equalsIgnoreCase("dinheiro")) {
+            System.out.println("Pagamento Aprovado");
+        } else {
+            System.out.println("Pagamento não autorizado");
+        }
+    }
+
+    public void recibo() {
+        for (int i = 0;i>=0; ) {
+            System.out.println("Pedido: " + rnd.nextInt());
+            break;
+        }
+        System.out.println("Pedido realizado com sucesso");
+    }
+
+}
+
+
+
+
